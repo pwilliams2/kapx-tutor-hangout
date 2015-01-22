06ae938931fc5cd4bb472b170f89039c633a0a93
Preston Williams <pwilliams2@kaplan.edu>
2015-01-21 21:37:10 -0500

    initial commit

--- models/models.py
+++ models/models.py
@@ -2,9 +2,23 @@ __author__ = 'admin'
 
 from google.appengine.ext import ndb
 
-class Tutor(ndb.Model):
+class TutorSubjects(ndb.Model):
+    id = ndb.StringProperty()
+    subjects = ndb.StringProperty()
+    tutor_name = ndb.StringProperty(indexed=False)
+    create_date = ndb.DateTimeProperty(auto_now_add=True)
+
+
+class TutorHangoutSessions(ndb.Model):
     id = ndb.StringProperty()
     subjects = ndb.StringProperty()
     gid = ndb.StringProperty()
-    name = ndb.StringProperty(indexed=False)
-    create_date = ndb.DateTimeProperty(auto_now_add=True)
+    tutor_name = ndb.StringProperty(indexed=False)
+    start = ndb.DateTimeProperty(auto_now_add=True)
+    end = ndb.DateTimeProperty()
+
+class HangoutSubjects(ndb.Model):
+    subject = ndb.StringProperty()
+    isAvailable = ndb.BooleanProperty(indexed=False)
+    last_modified = ndb.DateTimeProperty(auto_now=True)
+    gid = ndb.StringProperty()
