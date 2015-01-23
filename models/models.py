701fd45399c9842510188dffb847ff1982bb372d
Preston Williams <pwilliams2@kaplan.edu>
2015-01-23 12:18:29 -0500

    refactor to use endpoints

--- models/models.py
+++ models/models.py
@@ -1,24 +1,26 @@
-__author__ = 'admin'
+__author__ = 'pwilliams'
 
+
+from endpoints_proto_datastore.ndb import EndpointsModel
 from google.appengine.ext import ndb
 
-class TutorSubjects(ndb.Model):
-    id = ndb.StringProperty()
+class TutorSubjects(EndpointsModel):
+    person_id = ndb.StringProperty() #participant.person.id
     subjects = ndb.StringProperty()
     tutor_name = ndb.StringProperty(indexed=False)
     create_date = ndb.DateTimeProperty(auto_now_add=True)
 
-
-class TutorHangoutSessions(ndb.Model):
-    id = ndb.StringProperty()
+class TutorHangoutSessions(EndpointsModel):
+    person_id = ndb.StringProperty()
     subjects = ndb.StringProperty()
     gid = ndb.StringProperty()
     tutor_name = ndb.StringProperty(indexed=False)
     start = ndb.DateTimeProperty(auto_now_add=True)
     end = ndb.DateTimeProperty()
 
-class HangoutSubjects(ndb.Model):
+class HangoutSubjects(EndpointsModel):
     subject = ndb.StringProperty()
     isAvailable = ndb.BooleanProperty(indexed=False)
     last_modified = ndb.DateTimeProperty(auto_now=True)
     gid = ndb.StringProperty()
+    image_url = ndb.StringProperty()
