ef91df62067710665d08a646f7184e4a7dce8714
Preston Williams <pwilliams2@kaplan.edu>
2015-01-25 15:41:45 -0500

    initial commit

--- models/models.py
+++ models/models.py
@@ -1,26 +1,30 @@
 __author__ = 'pwilliams'
 
-
-from endpoints_proto_datastore.ndb import EndpointsModel
 from google.appengine.ext import ndb
 
+from endpoints_proto_datastore.ndb import EndpointsModel
+
+
 class TutorSubjects(EndpointsModel):
+    _message_fields_schema = ('entityKey','person_id','subjects','tutor_name','create_date', 'last_modified', 'gid')
     person_id = ndb.StringProperty() #participant.person.id
     subjects = ndb.StringProperty()
-    tutor_name = ndb.StringProperty(indexed=False)
+    tutor_name = ndb.StringProperty()
     create_date = ndb.DateTimeProperty(auto_now_add=True)
+    last_modified = ndb.DateTimeProperty(auto_now=True)
+    gid = ndb.StringProperty()
 
 class TutorHangoutSessions(EndpointsModel):
     person_id = ndb.StringProperty()
     subjects = ndb.StringProperty()
     gid = ndb.StringProperty()
     tutor_name = ndb.StringProperty(indexed=False)
-    start = ndb.DateTimeProperty(auto_now_add=True)
-    end = ndb.DateTimeProperty()
+    start = ndb.DateTimeProperty(auto_now_add=True, indexed=False)
+    end = ndb.DateTimeProperty(indexed=False)
 
 class HangoutSubjects(EndpointsModel):
     subject = ndb.StringProperty()
-    isAvailable = ndb.BooleanProperty(indexed=False)
+    is_available = ndb.BooleanProperty(indexed=False)
     last_modified = ndb.DateTimeProperty(auto_now=True)
     gid = ndb.StringProperty()
-    image_url = ndb.StringProperty()
+    image_url = ndb.StringProperty(indexed=False)
