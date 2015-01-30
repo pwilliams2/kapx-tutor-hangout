cd2a8ee74643e6ec896cd11864eed87a08584e55
Preston Williams <pwilliams2@kaplan.edu>
2015-01-30 18:02:32 -0500

    initial commit

---
+++ endpoints_proto_datastore/__init__.py
@@ -0,0 +1,7 @@
+from endpoints_proto_datastore import ndb
+
+
+__all__ = [ndb]
+
+from endpoints_proto_datastore.utils import *
+__all__ += utils.__all__
---
+++ endpoints_proto_datastore/ndb/__init__.py
@@ -0,0 +1,10 @@
+__all__ = []
+
+from endpoints_proto_datastore.ndb.model import *
+__all__ += model.__all__
+
+from endpoints_proto_datastore.ndb.properties import *
+__all__ += properties.__all__
+
+from endpoints_proto_datastore.ndb.utils import *
+__all__ += utils.__all__
