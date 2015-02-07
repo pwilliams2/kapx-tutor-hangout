150644b5a781786070aafe5172679fe624a756e7
Preston Williams <pwilliams2@kaplan.edu>
2015-02-07 17:38:47 -0500

    refactor to remove EP calls

--- web/handlers.py
+++ web/handlers.py
@@ -1,9 +1,8 @@
 import json
 import datetime
-from google.appengine.api.datastore_errors import BadValueError
 
+from google.appengine.api.datastore_errors import BadValueError
 from google.appengine.runtime.apiproxy_errors import OverQuotaError
-import jsonpickle
 
 import tutor_hangouts_api as hapi
 from apiclient import discovery
@@ -16,7 +15,7 @@ def remove_stale_sessions():
     """ When a tutor closes their H-O, we need to remove them from the available subjects """
     autolog("remove_stale_sessions")
     now = datetime.datetime.now()
-    del_list = TutorSubjects.query().fetch()
+    del_list = TutorSubjects.query(ancestor=hapi.SUBJECTS_PARENT_KEY).fetch()
     keys = []
     for item in del_list:
         delta = now - item.last_modified
@@ -26,16 +25,15 @@ def remove_stale_sessions():
     ndb.delete_multi(keys)
 
 
-def assign_available_tutors(avail_tutors_list, subjects_list):
-    """ Assign the tutors in the avail_ttutors_list to the  subjects
+def assign_available_tutors(avail_tutors, subjects_list):
+    """ Assign the tutors in the avail_tutors_list to the  subjects
     :param avail_tutors_list: available tutors and their subjects
     :param subjects_list: the tutored subjects
     :return:
     """
-    avail_tutors = avail_tutors_list['items']
     avail_subjects = []
     for tutor in avail_tutors:
-        json_tutor_subjects = json.loads(tutor['subjects'])
+        json_tutor_subjects = json.loads(tutor.subjects)
         for tutor_subject in json_tutor_subjects:
             subject = tutor_subject['subject']
             avail_subjects.append(subject)
@@ -43,13 +41,11 @@ def assign_available_tutors(avail_tutors_list, subjects_list):
         done = []  # Subjects that are already updated.
         for avail_subject in avail_subjects:
             for subject in subjects_list:
-                subject_key = subject.key.urlsafe()
-
                 if subject.subject in done:
                     continue
 
-                if avail_subject == subject.subject and tutor['participants_count'] < tutor['max_participants']:
-                    subject.gid = tutor['gid']
+                if avail_subject == subject.subject and tutor.participants_count < tutor.max_participants:
+                    subject.gid = tutor.gid
                     subject.is_available = True
 
                     if avail_subject not in done:
@@ -60,32 +56,32 @@ def assign_available_tutors(avail_tutors_list, subjects_list):
 
                 try:
                     subject.put()
-                except:
-                    OverQuotaError
-                autolog('Over Quota Error, bypassing for now')
+                except OverQuotaError, e:
+                    autolog('Over Quota Error, bypassing for now: ' + str(e))
 
 
 def update_subjects():
     '''Update the subjects table to reflect the availability of the current tutor'''
 
-    subjects_list = HangoutSubjects.query().fetch()
+    subjects_list = HangoutSubjects.query(ancestor=hapi.SUBJECTS_PARENT_KEY).fetch()
+    autolog('tutor_list' % subjects_list)
 
     # Retrieve Available Tutors and their subjects
-    avail_tutors_list = TutorSubjects().query().fetch()
+    avail_tutors = TutorSubjects.query(ancestor=hapi.TUTOR_SUBJECTS_PARENT_KEY).fetch()
+    print 'avail_tutors: ', avail_tutors
 
-    if not 'items' in avail_tutors_list:  # There are no tutors available
+    if not avail_tutors:  # There are no tutors available
         autolog('no avail_tutors_list')
         for subject in subjects_list:  # Set all subjects to unavailable
             subject.is_available = False
             subject.gid = None
             try:
                 subject.put()
-            except:
-                OverQuotaError
-            autolog('Over Quota Error, bypassing for now')
+            except OverQuotaError, e:
+                autolog('Over Quota Error, bypassing for now: ' + str(e))
     else:
-        autolog('avail_tutors_list has tutors %s' % avail_tutors_list)
-        assign_available_tutors(avail_tutors_list, subjects_list)
+        autolog('avail_tutors_list has tutors %s' % avail_tutors)
+        assign_available_tutors(avail_tutors, subjects_list)
 
 
 class PublishHandler(BaseHandler):
@@ -93,43 +89,36 @@ class PublishHandler(BaseHandler):
         self.publish_tutor()
         update_subjects()
 
-    def get_tutor_subjects_entity_key(self):
-        entity_key = self.request.get('entityKey')
-        if not entity_key:
-            ts_list = TutorSubjects.query(TutorSubjects.person_id == self.request.get('pid')).fetch(1, keys_only=True)
-
-            if ts_list:  # Found an existing tutor person_id
-                entity_key = ts_list[0].urlsafe()
-        return entity_key
 
     def publish_tutor(self):
         """ Insert/update the subjects for which the tutor is available """
 
-        discovery_url = '%s/discovery/v1/apis/%s/%s/rest' % (hapi.API_ROOT, hapi.API_NAME, hapi.VERSION)
-        service = discovery.build(hapi.API_NAME, hapi.VERSION, discoveryServiceUrl=discovery_url)
+        try:
+            ts = TutorSubjects.query(TutorSubjects.person_id == self.request.get('pid')).fetch(1)
 
-        count = int(self.request.get('count')) if self.request.get('count') else 0
-        max = int(self.request.get('maxParticipants')) if self.request.get('maxParticipants') else 1
+            count = int(self.request.get('count')) if self.request.get('count') else 0
+            max = int(self.request.get('maxParticipants')) if self.request.get('maxParticipants') else 1
 
-        tutor_body = {
-            "person_id": self.request.get('pid'),
-            "tutor_name": self.request.get('pName'),
-            "gid": self.request.get('gid'),
-            "subjects": self.request.get('subjects'),
-            "participants_count": count,
-            "max_participants": max
-        }
-
-        entity_key = self.get_tutor_subjects_entity_key()
-
-        if entity_key:
-            autolog("Updating tutor subject")
-            output = service.tutor_subjects().update(entityKey=entity_key, body=tutor_body).execute()
-        else:
-            autolog("New tutor")
-            output = service.tutor_subjects().insert(body=tutor_body).execute()
-
-        self.response.out.write(JSONEncoder().encode(output))
+            if ts:
+                autolog("Updating tutor subject")
+                ts[0].gid = self.request.get('gid')
+                ts[0].subjects = self.request.get('subjects')
+                ts[0].participants_count = count
+                ts[0].max_participants = max
+                ts_key = ts[0].put()
+            else:
+                autolog("New tutor")
+                ts = TutorSubjects(parent=hapi.TUTOR_SUBJECTS_PARENT_KEY,
+                                   person_id=self.request.get('pid'),
+                                   subjects=self.request.get('subjects'),
+                                   tutor_name=self.request.get('pName'),
+                                   gid=self.request.get('gid'),
+                                   max_participants=max,
+                                   participants_count=count)
+                ts_key = ts.put()
+            self.response.out.write(ts_key)
+        except Exception, e:
+            autolog('msg: ' + str(e))
 
 
 class HeartbeatHandler(BaseHandler):
@@ -140,11 +129,13 @@ class HeartbeatHandler(BaseHandler):
         discovery_url = '%s/discovery/v1/apis/%s/%s/rest' % (hapi.API_ROOT, hapi.API_NAME, hapi.VERSION)
         service = discovery.build(hapi.API_NAME, hapi.VERSION, discoveryServiceUrl=discovery_url)
 
+        # Search for the specified tutor id == pid
         ts_list = TutorSubjects.query(TutorSubjects.person_id == self.request.get('pid')).fetch()
 
         if not ts_list:  # hangout is no longer available
-            pass
-        else:  # Found an existing tutor person_id
+            remove_stale_sessions()  # cleanup closed sessions
+            update_subjects()  # Update available subjects
+        else:  # Found an existing tutor person_id, then update the count
             entity_key = ts_list[0].key.urlsafe()
             count = self.request.get("count") if self.request.get("count") else 1
             tutor_body = {
@@ -227,13 +218,13 @@ class SurveyHandler(BaseHandler):
         """ Post surveys  """
         try:
             survey = TutorSurveys(subject=self.request.get('subject'),
-                              tutor_id=self.request.get('tutor_id'),
-                              tutor_name=self.request.get('tutor_name'),
-                              knowledge=float(self.request.get('knowledge')),
-                              communications=float(self.request.get('communications')),
-                              overall=float(self.request.get('overall')),
-                              gid=self.request.get('gid'),
-                              comments=self.request.get('comments'))
+                                  tutor_id=self.request.get('tutor_id'),
+                                  tutor_name=self.request.get('tutor_name'),
+                                  knowledge=float(self.request.get('knowledge')),
+                                  communications=float(self.request.get('communications')),
+                                  overall=float(self.request.get('overall')),
+                                  gid=self.request.get('gid'),
+                                  comments=self.request.get('comments'))
             out = survey.put()
             self.response.out.write(out)
         except BadValueError, e:
@@ -253,7 +244,7 @@ class MainPage(BaseHandler):
 class SessionsPage(BaseHandler):
     def get(self):
         """ Get the list of TutorHangoutSessions """
-        sessions = TutorHangoutSessions.query().fetch()
+        sessions = TutorHangoutSessions.query(ancestor=hapi.TUTOR_SESSIONS_PARENT_KEY).fetch()
 
         template_data = {'sessions_query': sessions}
         self.render_template('views/sessions_report.html', **template_data)
